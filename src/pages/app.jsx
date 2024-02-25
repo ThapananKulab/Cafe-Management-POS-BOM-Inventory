828bff1f82beb690312f30a6fb8302d8c536b799
ThapananKulab <nicekrubma123@gmail.com>
2024-02-25 10:29:28 +0700

    update

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,10 +1,47 @@
+import Swal from 'sweetalert2';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
+
+import { useRouter } from 'src/routes/hooks';
 
 import { AppView } from 'src/sections/overview/view';
 
-// ----------------------------------------------------------------------
-
 export default function AppPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch('https://cafe-project-server11.onrender.com/api/authen', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        if (result.status === 'ok') {
+          // Possible implementation or comment
+        } else {
+          localStorage.removeItem('token');
+          Swal.fire({
+            icon: 'error',
+            title: 'กรุณา Login ก่อน',
+            text: result.message,
+          });
+          router.push('/');
+        }
+      } catch (error) {
+        console.error('Error:', error.status);
+      }
+    };
+    fetchData();
+  }, [router]);
+
   return (
     <>
       <Helmet>
