97065de6b30c7aa361f3ca30b1c7ff156786de19
ThapananKulab <nicekrubma123@gmail.com>
2024-03-08 22:37:41 +0700

    update

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -44,7 +44,8 @@ export default function LoginView() {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3333/api/login', {
+      // const response = await fetch('http://localhost:3333/api/login', {
+      const response = await fetch('https://cafe-project-server11.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
