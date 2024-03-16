25058f99eb4881508e407767795beedf45a9f033
ThapananKulab <nicekrubma123@gmail.com>
2024-03-16 23:52:35 +0700

    update

--- src/pages/add-user.jsx
+++ src/pages/add-user.jsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Button, Select, MenuItem, TextField, InputLabel, FormControl } from '@mui/material';
+import { Box, Button, Select, MenuItem, TextField, InputLabel, FormControl } from '@mui/material';
 
 import './style.css';
 
@@ -31,7 +31,7 @@ const UserForm = () => {
 
   const handleUserSubmit = async (e) => {
     e.preventDefault();
-  
+
     const userFormData = new FormData();
     userFormData.append('username', username);
     userFormData.append('password', password);
@@ -42,7 +42,7 @@ const UserForm = () => {
     userFormData.append('address', address);
     userFormData.append('role', role);
     userFormData.append('image', image); // Always append the image
-  
+
     try {
       const response = await axios.post(
         'https://cafe-project-server11.onrender.com/api/users/insertReact',
@@ -50,9 +50,9 @@ const UserForm = () => {
         userFormData,
         { headers: { 'Content-Type': 'multipart/form-data' } }
       );
-  
+
       console.log('Server Response:', response.data);
-  
+
       if (response.data.success) {
         Swal.fire({
           icon: 'success',
@@ -60,7 +60,7 @@ const UserForm = () => {
           showConfirmButton: false,
           timer: 1500,
         });
-  
+
         navigate('/add-user');
       } else if (response.data.username && username !== response.data.username) {
         Swal.fire({
@@ -78,7 +78,7 @@ const UserForm = () => {
       }
     } catch (error) {
       console.error('Error registering user:', error);
-  
+
       if (error.response && error.response.data) {
         console.log('Server Error Response:', error.response.data);
         const errorMessage = error.response.data.message;
@@ -96,116 +96,138 @@ const UserForm = () => {
       }
     }
   };
-  
-  
+
   return (
     <div className="form-container">
       <div className="container">
-        <Button
-          onClick={handleBack}
-          variant="outlined"
-          color="primary"
-          sx={{
-            position: 'absolute',
-            left: 6,
-            top: 25,
-            zIndex: 10,
-            opacity: 0.9,
-            '&:hover': {
-              opacity: 1,
-            },
-          }}
-        >
-          <Icon icon="mingcute:back-fill" /> กลับสู่หน้าหลัก
-        </Button>
         <div className="frame">
           <h2>เพิ่มบัญชีผู้ใช้</h2>
           <form onSubmit={handleUserSubmit}>
-            <TextField
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              label="Username"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              label="Password"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="text"
-              value={firstname}
-              onChange={(e) => setFirstname(e.target.value)}
-              label="First Name"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="text"
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-              label="Last Name"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              label="Phone"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <TextField
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              label="Address"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <FormControl fullWidth variant="outlined" required>
-              <InputLabel>Role</InputLabel>
-              <Select
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                label="Role"
-              >
-                {roles.map((userRole) => (
-  <MenuItem key={userRole.value} value={userRole.value}>
-    {userRole.label}
-  </MenuItem>
-))}
-              </Select>
-            </FormControl>
-            <br />
-            <br />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '5px',
+                marginBottom: '5px',
+              }}
+            >
+              {/* Row 1 - Username & Password */}
+              <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+                <div style={{ flex: 1 }}>
+                  <TextField
+                    autoFocus
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </div>
+                <div style={{ flex: 1 }}>
+                  <TextField
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </div>
+              </div>
+
+              <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+                <TextField
+                  type="text"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+                <TextField
+                  type="text"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+                <TextField
+                  type="text"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  label="Phone"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+                <FormControl fullWidth variant="outlined" required>
+                  <InputLabel>Role</InputLabel>
+                  <Select value={role} onChange={(e) => setRole(e.target.value)} label="Role">
+                    {roles.map((userRole) => (
+                      <MenuItem key={userRole.value} value={userRole.value}>
+                        {userRole.label}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </div>
+
+              <div style={{ flex: 1 }}>
+                <TextField
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </div>
+              <div style={{ flex: 1 }}>
+                <TextField
+                  multiline
+                  rows={2} // กำหนดความสูงโดยการเพิ่มจำนวนบรรทัด
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  label="Address"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </div>
+            </div>
             <input type="file" onChange={(e) => setImage(e.target.files[0])} required />
             <br />
             <br />
-            <Button type="submit" variant="outlined" color="primary">
-              Submit
-            </Button>
+            <Box display="flex" justifyContent="flex-end" gap={2} sx={{ mb: 2 }}>
+              <Button
+                onClick={handleBack}
+                variant="contained"
+                color="error"
+                size="large"
+                sx={{ width: '180px', fontSize: 28 }}
+              >
+                <Icon icon="material-symbols:cancel-outline" sx={{ fontSize: 28 }} />
+              </Button>
+              <Button
+                type="submit"
+                variant="outlined"
+                color="primary"
+                size="large"
+                sx={{ width: '180px', fontSize: 28 }}
+              >
+                <Icon icon="formkit:submit" sx={{ fontSize: 28 }} />
+              </Button>
+            </Box>
           </form>
         </div>
       </div>
